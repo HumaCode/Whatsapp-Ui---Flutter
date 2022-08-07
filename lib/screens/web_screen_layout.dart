fc6225848082b13eca4617976606dc9cc21f9e5e
humaidi <humaidizakaria1998@gmail.com>
2022-08-07 22:29:13 +0700

    Web Screen Layout Page - Displaying Mobile Contact List

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("This is web"),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // web profile bar
+                  // web search
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          // web screen
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
